bf7af5bd3c349ce757bd98c9bf5974f46abcaa97
정태승 <hi563@g.skku.edu>
2025-02-14 17:25:35 +0900

    feat: Calendar 도메인 엔티티 추가 및 Auth 도메인 엔티티 추가

--- src/main/java/com/elice/iliceworksbe/calendar/entity/EventParticipant.java
+++ src/main/java/com/elice/iliceworksbe/calendar/entity/EventParticipant.java
@@ -1,4 +1,33 @@
 package com.elice.iliceworksbe.calendar.entity;
 
-public class EventParticipant {
+import com.elice.iliceworksbe.auth.entity.User;
+import com.elice.iliceworksbe.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.AuditOverride;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "EVENT_PARTICIPANT")
+@AuditOverride(forClass = BaseEntity.class)
+public class EventParticipant extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_participant_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
---
+++ src/main/java/com/elice/iliceworksbe/common/constant/CalendarType.java
@@ -0,0 +1,7 @@
+package com.elice.iliceworksbe.common.constant;
+
+public enum CalendarType {
+    MEMBER,
+    TEAM,
+    OTHER // 법정공휴일 캘린더
+}
