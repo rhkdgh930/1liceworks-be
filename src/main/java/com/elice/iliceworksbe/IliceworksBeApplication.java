f0596bb50db617e239fc8093770ba46df822da89
정태승 <hi563@g.skku.edu>
2025-02-14 15:38:03 +0900

    Initial commit

---
+++ src/main/java/com/elice/iliceworksbe/IliceworksBeApplication.java
@@ -0,0 +1,13 @@
+package com.elice.iliceworksbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IliceworksBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IliceworksBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elice/iliceworksbe/IliceworksBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.iliceworksbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IliceworksBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
