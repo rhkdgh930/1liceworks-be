dfac7aa7862ef255f3ae833598ab0cca8fd101cf
명광호 <rhkdgh930@naver.com>
2025-02-18 01:33:52 +0900

    feat: PositionController, UserTypeController 인증 기능 추가

--- src/main/java/com/elice/iliceworksbe/team/web/PositionController.java
+++ src/main/java/com/elice/iliceworksbe/team/web/PositionController.java
@@ -1,5 +1,6 @@
 package com.elice.iliceworksbe.team.web;
 
+import com.elice.iliceworksbe.auth.model.UserDetailsImpl;
 import com.elice.iliceworksbe.common.exception.BaseResponse;
 import com.elice.iliceworksbe.common.exception.ErrorCode;
 import com.elice.iliceworksbe.team.dto.position.PositionRequestDto;
@@ -7,6 +8,7 @@ import com.elice.iliceworksbe.team.dto.position.PositionResponseDto;
 import com.elice.iliceworksbe.team.dto.position.PositionUpdateDto;
 import com.elice.iliceworksbe.team.service.PositionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,25 +21,31 @@ public class PositionController {
     private final PositionService positionService;
 
     @PostMapping
-    public BaseResponse<PositionResponseDto> postPosition(@RequestBody PositionRequestDto positionRequestDto) {
+    public BaseResponse<PositionResponseDto> postPosition(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody PositionRequestDto positionRequestDto) {
         PositionResponseDto postResponseDto = positionService.postPosition(positionRequestDto);
         return new BaseResponse<>(postResponseDto);
     }
 
     @GetMapping
-    public BaseResponse<List<PositionResponseDto>> getAllPositions() {
+    public BaseResponse<List<PositionResponseDto>> getAllPositions(
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
         List<PositionResponseDto> getResponseDtos = positionService.getAllPositions();
         return new BaseResponse<>(getResponseDtos);
     }
 
     @GetMapping("/{positionId}")
-    public BaseResponse<PositionResponseDto> getPosition(@PathVariable Long positionId) {
+    public BaseResponse<PositionResponseDto> getPosition(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long positionId) {
         PositionResponseDto getResponseDto = positionService.getPosition(positionId);
         return new BaseResponse<>(getResponseDto);
     }
 
     @PatchMapping("/{positionId}")
     public BaseResponse<PositionResponseDto> patchPosition(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
             @PathVariable Long positionId,
             @RequestBody PositionUpdateDto positionUpdateDto) {
         PositionResponseDto patchResponseDto = positionService.patchPosition(positionId, positionUpdateDto);
@@ -45,7 +53,9 @@ public class PositionController {
     }
 
     @DeleteMapping("/{positionId}")
-    public BaseResponse<String> deletePosition(@PathVariable Long positionId) {
+    public BaseResponse<String> deletePosition(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long positionId) {
         positionService.deletePosition(positionId);
         return new BaseResponse<>(ErrorCode.NO_CONTENT);
     }
