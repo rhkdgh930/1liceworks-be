dccef2d7006fcf316a49e70e61019913a46c5b7b
eunoia73 <soo97703@naver.com>
2025-02-25 11:39:57 +0900

    fix: sse 재연결시 읽지 않은 알림 존재 여부 전송

--- src/main/java/com/elice/iliceworksbe/notification/repository/NotificationRepository.java
+++ src/main/java/com/elice/iliceworksbe/notification/repository/NotificationRepository.java
@@ -12,13 +12,19 @@ import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findTop50ByUserIdAndCreatedAtAfterOrderByCreatedAtDesc(Long userId, LocalDateTime createdAt);
+
     @Modifying
     @Query("UPDATE Notification e SET e.isRead = true WHERE e.user.id = :userId AND e.isRead = false")
     void markAllAsReadByUserId(@Param("userId") Long userId);
+
     @Transactional
     @Modifying
     @Query("UPDATE Notification n SET n.isSent = :isSent WHERE n.id = :notificationId")
     void updateIsSent(@Param("notificationId") Long notificationId, @Param("isSent") boolean isSent);
+
     @Query("SELECT n FROM Notification n WHERE n.user.id = :userId AND n.isSent = false")
     List<Notification> findUnsentNotifications(@Param("userId") Long userId);
+
+    boolean existsByUserIdAndIsReadFalse(Long userId);
+
 }
