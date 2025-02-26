0a232c7af323db7bd2962638fd2d219cca4d3410
eunoia73 <soo97703@naver.com>
2025-02-26 13:32:09 +0900

    fix: checkEventReminder 페이징 처리 추가

--- src/main/java/com/elice/iliceworksbe/notification/repository/EventReminderRepository.java
+++ src/main/java/com/elice/iliceworksbe/notification/repository/EventReminderRepository.java
@@ -1,6 +1,8 @@
 package com.elice.iliceworksbe.notification.repository;
 
 import com.elice.iliceworksbe.notification.entity.EventReminder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +12,13 @@ import java.util.List;
 
 public interface EventReminderRepository extends JpaRepository<EventReminder, Long> {
     @Query("SELECT e FROM EventReminder e WHERE e.notifyTime BETWEEN :start AND :end")
-    List<EventReminder> findByNotifyTimeBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+    Page<EventReminder> findByNotifyTimeBetween(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            Pageable pageable
+    );
+
     List<EventReminder> findAllByEventId(Long eventId);
+
     void deleteByEventId(Long eventId);
 }
